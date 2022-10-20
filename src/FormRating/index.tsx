2e26591e13449170ae69f8e773748c0010cdefe7
doinki <doinki94@gmail.com>
2022-10-21 05:46:04 +0900

    feat: Add FormRating

---
+++ src/FormRating/index.tsx
@@ -0,0 +1,39 @@
+import type { RatingProps } from '@mui/material';
+import Rating from '@mui/material/Rating';
+import type {
+  FieldPath,
+  FieldValues,
+  UseControllerProps,
+} from 'react-hook-form';
+import { useController } from 'react-hook-form';
+
+export type FormRatingProps<
+  TFieldValues extends FieldValues,
+  TName extends FieldPath<TFieldValues>
+> = Omit<
+  RatingProps,
+  'defaultValue' | 'name' | 'value' | 'onBlur' | 'onChange'
+> &
+  UseControllerProps<TFieldValues, TName>;
+
+const FormRating = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(
+  props: FormRatingProps<TFieldValues, TName>
+) => {
+  const { name, control, defaultValue, rules, shouldUnregister, ...rest } =
+    props;
+
+  const { field } = useController({
+    name,
+    control,
+    defaultValue,
+    rules,
+    shouldUnregister,
+  });
+
+  return <Rating {...rest} {...field} />;
+};
+
+export default FormRating;
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export { default as FormRating } from './FormRating';
+export * from './FormRating';
 export { default as FormTextField } from './FormTextField';
 export * from './FormTextField';
