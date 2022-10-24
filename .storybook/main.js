457ddd180ac0f233e56f5b258c57d68df4d2d283
doinki <doinki94@gmail.com>
2022-10-25 08:16:56 +0900

    Bump dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
+  core: { builder: 'webpack5' },
   framework: '@storybook/react',
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
 };
