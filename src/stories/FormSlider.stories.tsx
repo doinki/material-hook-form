d7976c1d6b9ab3d577b50547d3c462986a9dbf2a
doinki <doinki94@gmail.com>
2022-10-25 06:35:11 +0900

    feat: Add FormSlider

---
+++ src/stories/FormSlider.stories.tsx
@@ -0,0 +1,15 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react';
+
+import FormSlider from '../FormSlider';
+
+export default {
+  component: FormSlider,
+  title: 'FormSlider',
+} as ComponentMeta<typeof FormSlider>;
+
+const Template: ComponentStory<typeof FormSlider> = (args) => {
+  return <FormSlider {...args} />;
+};
+
+export const Basic = Template.bind({});
+Basic.args = { defaultValue: 50 };
