29e647ac79dbcbfbb3b614c5b816d28957827fe4
doinki <doinki94@gmail.com>
2022-10-28 23:26:13 +0900

    feat: Update Form

--- src/Form/index.tsx
+++ src/Form/index.tsx
@@ -1,25 +1,37 @@
 import type { BoxProps } from '@mui/material/Box';
 import Box from '@mui/material/Box';
-import type { FC, FormEventHandler, KeyboardEventHandler } from 'react';
+import type {
+  ElementType,
+  FC,
+  FormEventHandler,
+  KeyboardEventHandler,
+} from 'react';
 
 export interface FormProps extends BoxProps<'form'> {
   /**
-   * @default true
+   * @default 'form'
    */
-  preventEnterSubmit?: boolean;
+  component?: ElementType;
+  /**
+   * @default false
+   */
+  disableEnterSubmit?: boolean;
 }
 
+/**
+ * @see [React Box](https://mui.com/material-ui/react-box/#main-content)
+ */
 const Form: FC<FormProps> = (props) => {
   const {
     component = 'form',
     onKeyDown,
     onSubmit,
-    preventEnterSubmit = true,
+    disableEnterSubmit = false,
     ...rest
   } = props;
 
   const handleKeyDown: KeyboardEventHandler<HTMLFormElement> = (e) => {
-    if (preventEnterSubmit && e.key === 'Enter') {
+    if (disableEnterSubmit && e.key === 'Enter') {
       e.preventDefault();
     }
 
