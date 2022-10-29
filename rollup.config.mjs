951e2cc93615308d97f75164080b3c00f6f0ccad
doinki <doinki94@gmail.com>
2022-10-29 09:50:31 +0900

    feat: Add rollup

---
+++ rollup.config.mjs
@@ -0,0 +1,47 @@
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+const extensions = ['.js', '.jsx', '.ts', '.tsx'];
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const rollupConfig = {
+  external: [/node_modules/],
+  input: 'src/index.ts',
+  output: [
+    {
+      dir: 'lib/cjs',
+      format: 'cjs',
+      interop: 'esModule',
+      preserveModules: true,
+    },
+    { dir: 'lib', format: 'esm', preserveModules: true },
+  ],
+  plugins: [
+    nodeResolve({ extensions }),
+    babel({
+      babelHelpers: 'runtime',
+      extensions,
+      plugins: [
+        ['@babel/plugin-transform-runtime', { version: '^7.20.0' }],
+        '@emotion',
+      ],
+      presets: [
+        '@babel/preset-env',
+        [
+          '@babel/preset-react',
+          {
+            importSource: '@emotion/react',
+            runtime: 'automatic',
+          },
+        ],
+        '@babel/preset-typescript',
+      ],
+    }),
+    terser(),
+  ],
+};
+
+export default rollupConfig;
